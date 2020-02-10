8f10899af1b4eac3ef10e1c05b48247bc957bc09
Eva Fiedler <evafiedler@students.berkeley.net>
2020-02-10 14:35:12 -0800

    Initial commit

---
+++ src/BinaryTree.java
@@ -0,0 +1,144 @@
+import java.util.*;
+
+public class BinaryTree {
+    Nodde root;
+
+    private Nodde addRecursive(Nodde current, int value) {
+        if (current == null) {
+            return new Nodde(value);
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            // value already exists
+            return current;
+        }
+
+        return current;
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private BinaryTree createBinaryTree() {
+        BinaryTree bt = new BinaryTree();
+
+//        bt.add(6);
+//        bt.add(4);
+//        bt.add(8);
+//        bt.add(3);
+//        bt.add(5);
+//        bt.add(7);
+//        bt.add(9);
+
+        return bt;
+    }
+
+    private boolean containsNodeRecursive(Nodde current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == current.value) {
+            return true;
+        }
+        return value < current.value
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+
+    public boolean containsNode(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    private Nodde deleteRecursive(Nodde current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+
+            if (current.right == null) {
+                return current.left;
+            }
+    
+            if (current.left == null) {
+                return current.right;
+            }
+
+            int smallestValue = findSmallestValue(current.right);
+            current.value = smallestValue;
+            current.right = deleteRecursive(current.right, smallestValue);
+            return current;
+        }
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+
+    private int findSmallestValue(Nodde root) {
+        return root.left == null ? root.value : findSmallestValue(root.left);
+    }
+
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
+    }
+
+    public void traverseInOrder(Nodde node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder(Nodde node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Nodde node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.value);
+        }
+    }
+
+    public void traverseLevelOrder() {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Nodde> nodes = new LinkedList<>();
+        nodes.add(root);
+
+        while (!nodes.isEmpty()) {
+
+            Nodde node = nodes.remove();
+
+            System.out.print(" " + node.value);
+
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+
+            if (node.right!= null) {
+                nodes.add(node.right);
+            }
+        }
+    }
+
+}
---
+++ src/Nodde.java
@@ -0,0 +1,11 @@
+public class Nodde {
+    int value;
+    Nodde left;
+    Nodde right;
+
+    Nodde(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
